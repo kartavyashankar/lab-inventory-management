680695f734bc70e8467be1309168e7879c651d31
Kartavya Shankar <kartavyashankar@gmail.com>
2024-12-03 07:01:48 +0530

    Remove dependency-injector

--- client/ClientContainer.py
+++
@@ -1,7 +0,0 @@
-from dependency_injector import containers, providers
-
-from client.DBClient import DBClient
-
-
-class ClientContainer(containers.DeclarativeContainer):
-    db_client = providers.Singleton(DBClient)
